a1b4c183609ab2b960cf2d4cf8dca849c77f364d
Ahyoung Oh <fouros95@gmail.com>
2021-04-19 17:43:42 +0900

    self-study post

---
+++ _posts/2021-04-19-second-post.md
@@ -0,0 +1,13 @@
+---
+title: "코로나19의 사회과학&#58; Part 3. 인과관계를 이해하는 분석틀"
+
+categories:
+  - Self-study
+tags:
+  - research-methods
+---
+
+##### 박지용 교수님의 [코로나19의 사회과학](https://www.youtube.com/watch?v=yS-N4YfZg0s&list=PLKKkeayRo4PU0fpXF_N3rQx6DdJqiie_9&index=5)을 보고 정리한 것
+
+<br/>
+![](/assets/images/4-19.png){: .align-center}
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,4 +4,10 @@ var store = [{
         "tags": ["Blog"],
         "url": "/blog/first-post/",
         "teaser": null
+      },{
+        "title": "코로나19의 사회과학&#58; Part 3. 인과관계를 이해하는 분석틀",
+        "excerpt":"박지용 교수님의 코로나19의 사회과학을 보고 정리한 것       ","categories": ["Self-study"],
+        "tags": ["research-methods"],
+        "url": "/self-study/second-post/",
+        "teaser": null
       }]
