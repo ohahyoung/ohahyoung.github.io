743568d2257d61198997a2e2300dfb531d49f67a
Ahyoung Oh <fouros95@gmail.com>
2021-04-21 13:28:39 +0900

    second-post-modified

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "코로나19의 사회과학&#58; Part 3. 인과관계를 이해하는 분석틀",
-        "excerpt":"박지용 교수님의 코로나19의 사회과학을 보고 정리한 것       ","categories": ["Self-study"],
+        "excerpt":"박지용 교수님의 [코로나19의 사회과학]을 보고 정리한 것 (Part3)       Causal Inference(인과추론)은 수많은 요소들이 알게 모르게 상호작용하고 있는 사회과학에서 굉장히 어려운 일이다. 근본적으로 어떤 일이 일어나지 않았다면에 대한 결과를 눈으로 볼 수 없고, 1) 선택편향: 실험 그룹과 통제 그룹이 비교 가능한 집단인가 2) 역 인과관계: 무엇이 원인(선행변수)이고 무엇이 결과(후행변수)인가 라는 난적을 가지고...","categories": ["Self-study"],
         "tags": ["research-methods"],
         "url": "/self-study/second-post/",
         "teaser": null
